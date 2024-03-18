f26f9c50bc747d749dc4b625cd3160a1d9534c4f
Toshevska Martina <martina.tosevska.95@gmail.com>
2024-03-18 11:41:03 +0100

    Modify train function to work with 2D train mask

--- IIS_2023_2024/av2/model_utils.py
+++ IIS_2023_2024/av2/model_utils.py
@@ -1,10 +1,15 @@
 def train(model, dataset, optimizer, criterion, epochs=5):
+    if len(dataset.train_mask.shape) > 1:
+        train_mask = dataset.train_mask[:, 0]
+    else:
+        train_mask = dataset.train_mask
+
     for epoch in range(epochs):
         model.train()
         optimizer.zero_grad()
         out = model(dataset.x, dataset.edge_index)
-        loss = criterion(out[dataset.train_mask],
-                         dataset.y[dataset.train_mask])
+        loss = criterion(out[train_mask],
+                         dataset.y[train_mask])
         loss.backward()
         optimizer.step()
 
